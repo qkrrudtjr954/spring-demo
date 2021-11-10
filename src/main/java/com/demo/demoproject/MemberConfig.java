2d53c3bf7d51780f5055c0846fdb99abbd5b0fdb
kyungseok.park <qkrrudtjr954@gmail.com>
2021-11-10 13:03:26 +0900

    old jdbc 코드를 활용한 디비 연결

--- src/main/java/com/demo/demoproject/MemberConfig.java
+++ src/main/java/com/demo/demoproject/MemberConfig.java
@@ -1,13 +1,23 @@
 package com.demo.demoproject;
 
+import com.demo.demoproject.repository.JdbcMemberRepository;
 import com.demo.demoproject.repository.MemberRepository;
-import com.demo.demoproject.repository.MemoryMemberRepository;
 import com.demo.demoproject.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class MemberConfig {
+    private DataSource dataSource;
+
+    @Autowired
+    public MemberConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
     @Bean
     public MemberService memberService() {
         return new MemberService(memberRepository());
@@ -15,7 +25,8 @@ public class MemberConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+//        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 
 
